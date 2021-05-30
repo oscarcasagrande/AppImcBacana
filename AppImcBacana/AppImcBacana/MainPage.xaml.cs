6c80288791972f35c19188faa1e1991f08d8497d
oscarlc <oscarlc@fastshop.com.br>
2021-05-30 19:51:19 -0300

    Add project files.

---
+++ AppImcBacana/AppImcBacana/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppImcBacana
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double peso = Convert.ToInt32(Peso.Text);
+            double altura = Convert.ToInt32(Altura.Text);
+            double imc = (peso / (altura / 100 * altura / 100));
+
+            string resultado = string.Format("Seu IMC é {0:N} %", imc);
+
+            Resultado.Text = resultado;
+        }
+
+        private void Button_Clicked_Go_To_WebSite(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://oscarcasagrande.com.br/?soruce=igti-teste-pratico"));
+        }
+    }
+}
